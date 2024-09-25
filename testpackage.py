0b47c5d81ee2966b10fc51f473dac18c2d97659d
Aa7009975 <aa7009975@gmail.com>
2024-09-25 18:51:20 +0530

    Add files via upload

---
+++ testpackage.py
@@ -0,0 +1,10 @@
+from package.Geometry import reactangleArea,circle
+from package.Arithmetic import addition,mul,divi,subtract
+
+print("area of a ractangle is : ",reactangleArea(3,5))
+print("area of a circle is : ", circle(2))
+
+print("addition : ",addition(3,5))
+print("addition : ",mul(3,5))
+print("addition : ",divi(6,3))
+print("addition : ",subtract(6,3))
